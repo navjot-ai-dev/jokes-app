a6116ebc53d170ef19dc10b69c9151bd5c0fad3d
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-06-29 16:53:59 +0530

    first commit

---
+++ jokes.app/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const App = () => {
+   const [que, setfirst] = useState('')
+   const [ans, setans] = useState('')
+
+  return (
+  <div className="bg-[url('https://img.magnific.com/free-vector/emoji-summer-background_79603-1589.jpg?semt=ais_hybrid&w=740&q=80')] bg-cover bg-center h-screen flex justify-center items-center">
+       <div className='h-3/4 w-1/2 bg-amber-200 rounded-3xl'>
+
+       </div>
+
+    </div>
+  )
+}
+
+export default App
